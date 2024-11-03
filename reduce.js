1b44946690e64c9416cf7eddcb1fb3ac44cc2f3b
contactjual <contactjual@gmail.com>
2024-11-03 12:15:33 +0600

    second commit

---
+++ reduce.js
@@ -0,0 +1,6 @@
+const numbers = [4, 56, 7, 9, 7];
+const total = numbers.reduce((previous, current) => previous + current, 0)
+console.log(total);
+
+// previous - protita sonka jog kore kore jog fol 
+// current - next sonka ti
